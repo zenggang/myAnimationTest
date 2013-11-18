fcb36283f840dce58be5be902f62737f636eb8bb
zenggang <zenggang2008@gmail.com>
2013-11-18 10:51:14 +0800

    first

---
+++ myAnimationTest/myAnimationTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  myAnimationTest
+//
+//  Created by gang zeng on 13-11-18.
+//  Copyright (c) 2013年 gang zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ myAnimationTest/myAnimationTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myAnimationTest
+//
+//  Created by gang zeng on 13-11-18.
+//  Copyright (c) 2013年 gang zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
